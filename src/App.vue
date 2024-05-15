135d093348293008eae767d4261983866966bee4
pedrocbp <pedromiguelboce@gmail.com>
2024-05-15 19:58:00 -0300

    Update products components

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,74 @@
-<script setup>
-import {ref} from 'vue'
-
-let num = ref(0)
-
-</script>
 <template>
-
-<nav class="navbar navbar-expand-lg navbar-dark border-bottom border-body">
-  <div class="container-fluid">
-    <router-Link class="navbar-brand" to="/"><h1 class="logo">VIMA<span>STORE</span></h1></router-Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-Link class="nav-link" aria-current="page" to="/">Inicio</router-Link>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body">
+    <div class="container-fluid">
+      <router-Link class="navbar-brand" to="/"><h1 class="logo">VIMA<span>STORE</span></h1></router-Link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-Link class="nav-link" aria-current="page" to="/">Inicio</router-Link>
+          </li>
+          <li class="nav-item">
+            <router-Link class="nav-link" aria-current="page" to="/about">Sobre</router-Link>
+          </li>
+          <li class="nav-item">
+            <router-Link class="nav-link" aria-current="page" to="/product">
+              <span class="material-symbols-outlined">Produto-teste</span>
+            </router-Link>
+          </li>
+          <li class="nav-item">
+            <router-Link class="nav-link" aria-current="page" to="/contact">Contato</router-Link>
+          </li>
+          <li class="nav-item nav-car ml-auto">
+          <router-Link class="nav-link" aria-current="page" to="/cart"><i class="pi pi-shopping-cart ml-auto" style="font-size: 1.5rem"></i></router-Link>
         </li>
-        <li class="nav-item">
-          <router-Link class="nav-link" aria-current="page" to="/about">Sobre</router-Link>
-        </li>
-        <li class="nav-item">
-          <router-Link class="nav-link" aria-current="page" to="/contact">Contato</router-Link>
-        </li>
-        <li class="nav-item nav-car ml-auto">
-          <router-Link class="nav-link" aria-current="page" to="/Car"><i class="pi pi-shopping-cart ml-auto" style="font-size: 1.5rem"></i></router-Link>
-        </li>
-      </ul>
+        </ul>
+      </div>
     </div>
+  </nav>
+  <div class="main">
+    <router-view></router-view>
   </div>
-</nav>
-<div class="main">
-  <router-view></router-view>
-</div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+let num = ref(0);
+</script>
+
 <style>
-
-
-.navbar {
-  background-color: #00474F;
-}
-
-footer{
+footer {
   position: fixed;
-  bottom:  0;
+  bottom: 0;
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   height: 50px;
-  background-color: #00474F;
-  font-size:small;
 }
 
+.logo {
+  display: flex;
+  align-items: center;
+  color: var(--tex-color);
+  font-size: 28px;
+  font-weight: bold;
+}
 
+.logo i {
+  color: var(--main-color);
+  font-size: 32px;
+  margin-right: 5px;
+}
 
+.logo {
+  color: #34e7f8;
+}
+
+.logo span {
+  color: #fff;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -12,19 +12,25 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
     {
       path: '/contact',
       name: 'contact',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/ContactView.vue')
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: () => import('../views/ProductView.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      
+      component: () => import('../views/CartView.vue')
     }
+
   ]
 })
 
